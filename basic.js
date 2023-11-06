fa515082299912ec32d4131f465c711284e57108
Aliz Shrestha <98611497+thealizstha@users.noreply.github.com>
2023-11-06 22:03:58 +0545

    math_obj

---
+++ basic.js
@@ -0,0 +1,14 @@
+// Math object 
+
+document.write(Math.PI,"<br>");   // pi value
+document.write(Math.E,"<br>");   // exponential
+
+ var x = Math.round(4.7);     // round numbers
+ document.write(x,"<br>");
+
+ var y = Math.sqrt(4)    //square root
+ document.write(y);
+ 
+ //Math.pow
+ //Math.ceil
+ //Math.floor etc.
